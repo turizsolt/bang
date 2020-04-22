ea547b0216fae59dfcd2ea2e37dfc854c84c7a2a
turizsolt <turi.zsolt.m@gmail.com>
2020-04-22 22:01:12 +0200

    chore: init repo

---
+++ test/1.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('Hello', () => {
+  it('World', () => {
+    console.log('hello world');
+    expect(true).equals(true);
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+// Optimizes duplicates in splitted bundles
+const webpack = require('webpack');
+// creates index.html file by a template index.ejs
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// cleans dist folder
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// copies the assets folder into dist folder
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// output folder location
+const distFolder = './build';
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/game.html',
+      filename: 'game.html'
+    }),
+    new CopyWebpackPlugin([{ from: 'src/assets', to: 'assets' }]),
+    new CopyWebpackPlugin([{ from: 'src/html/*.html', to: '.', flatten: true }])
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: distFolder,
+    host: '0.0.0.0',
+    //public: 'zsiri.eu:8080',
+    public: 'localhost:8080'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, distFolder)
+  }
+};
